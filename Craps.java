745e04bba0cdf90e555de96827e8b2e95b814ec7
Kaylin Yagura <kyagura24@students.polytechnic.org>
2022-01-11 11:42:22 -0800

    Finished with a few errors help

--- Craps.java
+++ Craps.java
@@ -13,7 +13,6 @@ public class Craps
     // instance variables - replace the example below with your own
     private int x;
 
-    
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         System.out.println("Let's Play Craps!");
@@ -30,20 +29,103 @@ public class Craps
             System.out.println("4. b. or you roll a 7 and lose.");
 
         }
-        
-        
-        
+
+        Dice d = new Dice(6, 2);
+
         boolean game_on = true;
-        
+
         while(game_on){
-           System.out.println("Good Luck");
-           System.out.println("Press <Enter> to roll the dice");
-           String pause = in.nextLine();
-           
-           
-            
+            System.out.println("Good Luck");
+            System.out.println("Press <Enter> to roll the dice");
+            String pause = in.nextLine();
+            int num1 = d.roll();
+            System.out.println("Your first roll is: " + num1);
+
+            if(num1 == 7 || num1 == 11){
+                System.out.println("Congratulations! You win!");
+                System.out.println("Play again? (Y/N)");
+                String answer = in.nextLine();
+                if(instructions.equals("Y") || instructions.equals("y") || instructions.equals("yes") || instructions.equals("Yes")){
+                    game_on = true;
+                }
+                else{
+                    System.out.println("Thank you for playing!  Goodbye!");
+                    game_on = false;
+                    break;
+
+                }
+
+            }
+
+            else if(num1 == 2 || num1 == 3 || num1 == 12){
+                System.out.println("Sorry, you lose.");
+                System.out.println("Play again? (Y/N)");
+                String answer = in.nextLine();
+                if(instructions.equals("Y") || instructions.equals("y") || instructions.equals("yes") || instructions.equals("Yes")){
+                    game_on = true;
+                }
+                else{
+                    System.out.println("Thank you for playing!  Goodbye!");
+                    game_on = false;
+                    break;
+
+                }
+
+            }
+
+            else{
+                boolean roll = true;
+                System.out.println("That's your point.");
+                System.out.println("Let's see if you can roll that number again before you roll a seven!");
+                while(roll = true){
+                    System.out.println("Press <Enter> to roll the dice");
+                    String pause1 = in.nextLine();
+                    int num2 = d.roll();
+
+                    if(num2 == 7){
+                        roll = false;
+                        System.out.println("Sorry, you lose.  You rolled a 7.");
+                        System.out.println("Play again? (Y/N)");
+                        String answer = in.nextLine();
+                        if(instructions.equals("Y") || instructions.equals("y") || instructions.equals("yes") || instructions.equals("Yes")){
+                            game_on = true;
+                        }
+                        else{
+                            System.out.println("Thank you for playing!  Goodbye!");
+                            game_on = false;
+                            break;
+                            
+
+                        }
+                    }
+
+                    else if(num2 == num1){
+                        roll = false;
+                        System.out.println("Congratulations! You win!");
+                        System.out.println("Play again? (Y/N)");
+                        String answer = in.nextLine();
+                        if(instructions.equals("Y") || instructions.equals("y") || instructions.equals("yes") || instructions.equals("Yes")){
+                            game_on = true;
+                        }
+                        else{
+                            System.out.println("Thank you for playing!  Goodbye!");
+                            game_on = false;
+                            break;
+
+                        }
+                    }
+                    
+                    else{
+                        System.out.println("You rolled a " + num2);
+                        System.out.println("Keep rolling...");
+                        
+                    }
+
+                   
+                }
+            }
+
         }
 
-        
     }
 }
